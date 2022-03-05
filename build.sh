1e67c935ab7c8ded16da8dc704be14d91f049ff8
idk <hankhill19580@gmail.com>
2022-03-05 17:39:39 -0500

    update the index

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker run -it -w /home  --name php7-cli -v /home/idk/deb.i2pgit.org/htdocs:/home/ --rm phpdockerio/php71-cli php /home/index.php > index.html
+docker run -it -w /home  --name php7-cli -v $(pwd):/home/ --rm phpdockerio/php71-cli php /home/index.php > index.html
