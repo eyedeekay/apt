80a2ed5997340459304792c0b248fad23a66d4d1
idk <hankhill19580@gmail.com>
2022-03-05 16:07:14 -0500

    checkin unchecked file

--- find.sh
+++ find.sh
@@ -14,4 +14,10 @@ for f in $(git status -u | grep changelog); do
 	git push --all
 done
 
+for f in $(git status -u | grep changelog); do
+	git add -v "$f"
+	git commit -am "checkin $f $DATE"
+	git push --all
+done
+
 find ./dists -type f -name 'Contents*' -exec ./add.sh {} \;
