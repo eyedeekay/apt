52ad745823b159c57cefcff56c34c653600e14d9
idk <hankhill19580@gmail.com>
2022-03-05 16:02:19 -0500

    checkin unchecked file

--- find.sh
+++ find.sh
@@ -1,1 +1,17 @@
+#! /usr/bin/env sh
+
+DATE=$(date)
+
+for f in $(git status -u | grep dists); do
+	git add -v "$f"
+	git commit -am "checkin $f $DATE"
+	git push --all
+done
+
+for f in $(git status -u | grep changelog); do
+	git add -v "$f"
+	git commit -am "checkin $f $DATE"
+	git push --all
+done
+
 find ./dists -type f -name 'Contents*' -exec ./add.sh {} \;
