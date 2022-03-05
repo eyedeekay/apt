86a07fc9c8b11ece8f5f06245b9d6f1400194244
idk <hankhill19580@gmail.com>
2022-03-05 15:53:20 -0500

    add ./dists/wheezy/Contents-kfreebsd-amd64.gz

--- add.sh
+++ add.sh
@@ -2,8 +2,8 @@
 if echo $1 | grep '.git/'; then
 	echo "ignore git file"
 else
-	git add $1
-	echo "git commit -am \"add $1\""
-	git commit -am "add $1"
+	git add $1 && \
+		echo "git commit -am \"add $1\"" && \
+		git commit -am "add $1"
 	git push --all
 fi
