eaf735a8ebeec8914726ddb9ce613565383bd33e
idk <hankhill19580@gmail.com>
2022-07-06 01:37:10 -0400

    checkin M Wed Jul  6 01:37:06 AM EDT 2022

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env sh
-rm -f deb.i2p2.de.tar.gz
+#rm -f deb.i2p2.de.tar.gz
+rm -fr dists/unstable \
+ dists/oldoldstable \
+ dists/oldstable \
+ dists/stable
 wget -c -O deb.i2p2.de.tar.gz https://deb.i2p2.de/deb.i2p2.de.tar.gz
-tar --strip-components=4 -xvzf deb.i2p2.de.tar.gz
+tar --strip-components=4 --keep-directory-symlink -xvzf deb.i2p2.de.tar.gz
 docker run -it -w /home  --name php7-cli -v $(pwd):/home/ --rm phpdockerio/php71-cli php /home/index.php > index.html
 ./fakeod
--- dists/unstable/main/README.md
+++
@@ -1,9 +0,0 @@
-main
-========================
-
-- [.](.)
-- [source](source)
-- [binary-amd64](binary-amd64)
-- [Contents-amd64.gz](Contents-amd64.gz)
-- [README.md](README.md)
-- [Contents-amd64.bz2](Contents-amd64.bz2)
