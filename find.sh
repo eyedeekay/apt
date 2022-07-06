114fe29a629399e6e652d9fc74a3e35227ab59ed
idk <hankhill19580@gmail.com>
2022-07-06 01:25:39 -0400

    checkin dists/bionic/main/Contents-armhf.bz2 Wed Jul  6 01:25:00 AM EDT 2022

--- find.sh
+++ find.sh
@@ -2,24 +2,6 @@
 
 DATE=$(date)
 
-for f in $(git status -u | grep dists); do
-	git add -v "$f"
-	git commit -am "checkin $f $DATE"
-	git push --all
-done
-
-for f in $(git status -u | grep changelog); do
-	git add -v "$f"
-	git commit -am "checkin $f $DATE"
-	git push --all
-done
-
-for f in $(git status -u | grep pool); do
-	git add -v "$f"
-	git commit -am "checkin $f $DATE"
-	git push --all
-done
-
 for f in $(git status -u -s | sed 's|?? ||g'); do
 	git add -v "$f"
 	git commit -am "checkin $f $DATE"
