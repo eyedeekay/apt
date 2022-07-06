0636c84192dc2a2dd7eea551f6b20799d256712e
idk <hankhill19580@gmail.com>
2022-07-06 01:40:27 -0400

    checkin M Wed Jul  6 01:40:27 AM EDT 2022

--- find.sh
+++ find.sh
@@ -4,12 +4,12 @@ DATE=$(date)
 
 for f in $(git status -u -s | sed 's|?? ||g'); do
 	echo git add -v "$f"
-	#git add -v "$f"
+	git add -v "$f"
 	#sleep 1s
-	#echo git commit -am "checkin $f $DATE"
-	#git commit -am "checkin $f $DATE"
-	#echo git push --all
-	#git push --all
+	echo git commit -am "checkin $f $DATE"
+	git commit -am "checkin $f $DATE"
+	echo git push --all
+	git push --all
 done
 
 find ./dists -type f -name 'Contents*' -exec ./add.sh {} \;
